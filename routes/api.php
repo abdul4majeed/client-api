6cf6f7dc39a4b6bb14d99bff640b498521b68b95
AbdulMajeed <abdul4majeed@gmail.com>
2019-12-31 01:59:15 +0500

    change middleware cors implementation

--- routes/api.php
+++ routes/api.php
@@ -12,13 +12,13 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::group(['prefix' => 'auth','middleware' => 'cors'], function () {
-
-Route::post('register', 'Api\Auth\RegisterController@register');
-Route::post('login', 'Api\Auth\LoginController@login');
-Route::post('logout','Api\Auth\LoginController@logout');
-
-Route::get('get_countries','Api\UserController@countries');
+Route::group(['prefix' => 'auth'], function () {
+    
+    Route::post('login', 'Api\Auth\LoginController@login');
+    Route::post('register', 'Api\Auth\RegisterController@register');
+    Route::post('logout','Api\Auth\LoginController@logout');
+    
+    Route::get('get_countries','Api\UserController@countries');
 });
 
 Route::group(['middleware' => 'auth:api','cors'], function(){
